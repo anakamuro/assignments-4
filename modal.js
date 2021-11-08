6bf463ba64b3469eef8ea9a463203222398f8e91
Akihiko <anakamuro@earthlink.net>
2021-11-08 18:09:37 -0500

    first commit

--- modal.js
+++ modal.js
@@ -20,4 +20,40 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+const form = documemt.getElementById('form');
+
+form.addEventListener("submit", (e)=>{
+e.preventDefault();
+alert('Thank you for your submission');
+})
+
+window.onload = function(){
+const btnSubmit = document.getElementById('btn-submit');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+
+
+btnSubmit.addEventListener('click', function(event){
+
+  let message = [];
+  if(first.value == ""){
+   message.push("You need to write your first name");
+  }
+  if(last.value == ""){
+    message.push("You need to write your last name");
+   }
+   if(email.value == ""){
+    message.push("You need to write your email");
+   }
+   if(quantity.value == ""){
+    message.push("You need to write the quantity");
+   }
+   if(birthdate.value == ""){
+    message.push("You need to write your birthdate");
+   }
+})
+}
 
