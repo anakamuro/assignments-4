9361cc0ad39717e9354e8ac27c69d592190b95b6
Akihiko <anakamuro@earthlink.net>
2021-12-21 20:15:00 -0500

    tuesday commit

--- modal.js
+++ modal.js
@@ -18,7 +18,10 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 closeBtn.addEventListener("click", closeModal);
 
-
+var button = document.getElementById("close");
+button.onclick = function (){
+  modalbg.style.display = "none";
+}
 // close modal form 
 function closeModal() {
   modalbg.style.display = "none";
@@ -28,64 +31,114 @@ function closeModal() {
 function launchModal() {
   modalbg.style.display = "block";
 }
+/*
+function firstError(){
+  const firstInput = document.getElementById('first');
+  firstInput.innerHTML = "You need to write your first name";
+  firstInput.style.borderColor = red;
+}
+*/
 
+async function sayThanks() {
+  document.getElementById("thank-you").style.display = "inline-block";
+  document.getElementById("form").style.display = "none";
+  document.getElementById("thank-you").classList.add("#close");
+  console.log('calling');
+  const result = await closeModal();
+  console.log(result);
+  // expected output: "resolved"
+}
+function closeModal() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 3000);
+  });
+}
 const form = document.getElementById('form');
+
 
 form.addEventListener("submit", (e)=>{
 e.preventDefault();
-alert('Thank you for your submission');
-closeModal();
-})
+validate();
+}
+)
+
+
 
 function validate(){
-const btnSubmit = document.getElementById('btn-submit');
-const first = document.getElementById('first');
-const last = document.getElementById('last');
-const email = document.getElementById('email');
-const birthdate = document.getElementById('birthdate');
-const quantity = document.getElementById('quantity');
+const firstValue= document.getElementById('first').value.trim();
+const lastValue = document.getElementById('last').value.trim();
+const emailValue = document.getElementById('email').value.trim();
+const birthDateValue = document.getElementById('birthdate').value.trim();
+const quantityValue = document.getElementById('quantity').value.trim();
+const checkbox1Value = document.getElementById('checkbox1').value
 
-if((document.getElementById('checkbox1').checked == false)){
-  alert("You need to check the agreement")
+
+if(firstValue == ""){
+  errorHandler(first, 'First Name can not be blank');
+} else if (firstValue >= 2) {
+  errorHandler(first, 'First Name must be at least 2 characters');
+  } else if (!isNaN(firstValue)) {
+  errorHandler(first, 'First Name can not be number');
+} else {
+  successHandler(email);
 }
-if(quantity.value == ""){
-  alert("You need to write the quantity");
-  return false;
- }
-if((document.getElementById('location1').checked == false) && (document.getElementById('location2').checked == false) && (document.getElementById('location3').checked == false) && (document.getElementById('location4').checked == false) && (document.getElementById('location5').checked == false) && (document.getElementById('location6').checked == false)){
-  alert("you need to choose your location");
-  return false;
-}
-if(!isNaN(first.value) && !isNaN(last.value)){
-  alert("name should be character");
-  return false;
- }
-  if(first.value == ""){
-  alert("You need to write your first name");
-  return false;
+
+  if(lastValue == ""){
+    errorHandler(last, 'Last Name can not be blank');
+  } else if (lastValue >= 2) {
+    errorHandler(last, 'Last Name must be at least 2 characters');
+  } else if (!isNaN(lastValue)) {
+    errorHandler(last, 'Last Name can not be number');
+    } else {
+      successHandler(email);
+    }
+
+   if(emailValue == ""){
+    errorHandler(email, 'Your email can not be blank');
+   } else {
+    successHandler(email);
   }
-  if(last.value == ""){
-    alert("You need to write your last name");
-    return false;
-   }
-   if(email.value == ""){
-    alert("You need to write your email");
-    return false;
-   }
-   var at = email.indexOf("@");
-   var dot = email.lastIndexzOf(".");
+   
+  if(birthDateValue == ""){
+    errorHandler(birthdate, 'Your birthdate can not be blank');
+   } else {
+    successHandler(birthdate);
+  }
+  if(quantityValue == ""){
+    errorHandler(quantity, 'Your need to write how many times you attended');
+  } else if (isNaN(quantityValue)) {
+    errorHandler(last, 'Last Name can not be number');
+   } else {
+    successHandler(quantity);
+  }
+ function errorHandler(input, message){
+   const formData = input.parentElement;
+   const small = formData.querySelector('small');
+   small.innerText = message;
+   formData.className = 'formData error';
+   valid: false;
+ }
 
-   if(at<1 || dot < at + 2 || dot + 2 >= email.length){
-     alert("Not a valid email");
-     return false;
-   }
-   if(quantity.value == ""){
-    alert("You need to write the quantity");
-    return false;
-   }
-   if(birthdate.value == ""){
-    alert("You need to write your birthdate");
-    return false;
-   }
+ function successHandler(input){
+  const formData = input.parentElement;
+  formData.className = 'formData success'
+}
+if(document.querySelectorAll('[name=location]:checked').length == 0){
+  const formData = input.parentElement;
+  const small = formData.querySelector('small');
+  small.innerHTML = "hello";
+  formData.className = 'formData error';
+  return false;
+} 
+
+if(checkbox1Value.checked == false){
+  const must = document.getElementById('must');
+  must.innerHTML = "hello";
+  formData.className = 'formData error';
+  return false;
+}
+sayThanks();
 }
 
